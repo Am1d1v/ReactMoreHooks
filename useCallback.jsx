8230dea80228b5dad2e586dc08984325c70e0d67
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-06 23:03:58 +0300

    Add files via upload

--- useCallback.jsx
+++ useCallback.jsx
@@ -1,26 +1,25 @@
-import { useState } from "react";
+import React, { useState, useCallback} from "react";
 
 
-const CountButtons = ({onClick, count}) => {
+const CountButtons = React.memo(({onClick, count}) => {
     console.log('render', count)
-    
     return <button onClick={onClick}>{count}</button>
-}
+})
 
 function DualCounter(){
 
     const [count1, setCount1] = useState(0)
-    const increment1 = () => setCount1(count => count + 1);
+    const increment1 = useCallback(() => setCount1(count => count + 1), []);
 
     const [count2, setCount2] = useState(0)
-    const increment2 = () => setCount2(count => count + 1);
+    const increment2 = useCallback(() => setCount2(count => count + 1), []);
 
     console.log('render', `Count1: ${count1}, Count2: ${count2}`);
 
     return(
         <div>
-            <button count={count1} onClick={increment1}>{count1}</button>
-            <button count={count2} onClick={increment2}>{count2}</button>
+            <CountButtons count={count1} onClick={increment1}/>
+            <CountButtons count={count2} onClick={increment2}/>
         </div>
     )
 }
