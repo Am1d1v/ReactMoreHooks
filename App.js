136892e6e7560745e695250363f3e1df49d993ca
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-06 14:35:54 +0300

    Add files via upload
    useReducer()

--- App.js
+++ App.js
@@ -1,15 +1,71 @@
+import { useReducer } from 'react';
 import './App.css';
-import {DualCounter, CountButtons} from './useCallback';
-import Context from './Context';
-import Books from './components/Books';
+
+
+const limitRbg = (num) => (num < 0 ? 0 : num > 255 ? 255 : num)
+const step = 51;
+
+const reducer = (state, action) => {
+    switch(action.type){
+      case "INCREMENT_R":
+        return{
+          ...state,
+          r: limitRbg(state.r + step)
+        };
+      case "DECREMENT_R":
+        return{
+          ...state,
+          r: limitRbg(state.r - step)
+        };
+      case "INCREMENT_G":
+        return{
+          ...state,
+          r: limitRbg(state.g + step)
+        };
+      case "DECREMENT_G":
+        return{
+          ...state,
+          r: limitRbg(state.g - step)
+        };   
+      case "INCREMENT_B":
+        return{
+          ...state,
+          r: limitRbg(state.b + step)
+        };
+      case "DECREMENT_B":
+        return{
+          ...state,
+          r: limitRbg(state.b - step)
+        }; 
+
+    }
+}
 
 
 function App() {
+
+  const [{r, g, b}, dispatch] =  useReducer(reducer, {r: 0, g: 102, b: 204});
+
   return(
     <div className='App'>
-      <Context>
-        <Books />
-      </Context>
+
+      <h1 style={{color: `rgb(${r}, ${g}, ${b})`}}>Color Title</h1>
+      <div>
+        <span> </span>
+        <button onClick={''}>+</button>
+        <button onClick={''}>-</button>
+      </div>
+      <div>
+        <span> </span>
+        <button onClick={''}>+</button>
+        <button onClick={''}>-</button>
+      </div>
+      <div>
+        <span> </span>
+        <button onClick={''}>+</button>
+        <button onClick={''}>-</button>
+      </div>
+
     </div>
   )
 }
